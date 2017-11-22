e82734eb4a6a186ab2fd4fc38192e4323da9a158
Dan Morriss <daniel_morriss@yahoo.com.au>
2017-11-23 01:15:08 +1100

    Print nodename.

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+char myhostname[256];
 
 int main(int argc, char *argv[])
 {
     char buffer[80];
+    memset(&myhostname, 0, 256);
+    gethostname((char *) &myhostname, 255);
+
     setvbuf(stdout, NULL, _IONBF, 0);
-    printf("\n\nBMI Technology Menu\n\n");
+    printf("\n\nBMI Technology Menu\n");
+    printf("You are connected on node [%s]\n\n", myhostname);
 
     printf("\t1)	Download latest software release\n");
     printf("\t2)	Disconnect\n");
@@ -15,9 +22,11 @@ int main(int argc, char *argv[])
     printf("Enter your choice: ");
     memset(&buffer, 0, 80);
     fgets((char *) &buffer, 79, stdin);
+    printf("\n\n");
     printf("Your choice was: %s\n", buffer);
 
-		exit(1);
+    printf("\n");
+	exit(1);
 
 }
 
