64be39ff66dbdf66cf4d101dce36fa779f0d4598
Dan Morriss <daniel_morriss@yahoo.com.au>
2017-12-31 02:38:38 +1100

    Initial utility for converting UTF-8 ANSI files back to CP437. Testfile which is a result of that conversion.

---
+++ data/cp437.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+ * The triggering character for UNICODE is: 
+ *
+ *  226	E2	
+	Greek upper case letter gamma 
+ **/
+
+int main()
+{
+    bool unicode_on = false;
+    int unicode_byte_index = 0;
+    int rd = 0;
+    int i = 0;
+    uint16_t ubyte = 0;
+    unsigned char buffer[4096];
+
+    while (!rd) {
+        rd = read(0, &buffer, 4096);
+        fprintf(stderr, "rd = %d\n", rd);
+        for (i = 0; i < rd ; i++) {
+            if (unicode_on) {
+                    unicode_byte_index ++;
+                    switch(unicode_byte_index) {
+                        case 1:
+                            //ubyte = buffer[i];
+                            ubyte = (buffer[i] << 8);
+                            break;
+                        case 2:
+                            /* FIXME: not endian safe ! */
+                            //ubyte = ubyte + (buffer[i] << 8);
+                            ubyte += buffer[i]; 
+                            break;
+                        default:
+                            fprintf(stderr, "+++ Unicode character has bad length! = %d\n", unicode_byte_index);
+                            break;
+                        }
+                    if (unicode_byte_index == 2) {
+                        fprintf(stderr, "+++ ubyte = 0x%04x\n", ubyte);
+                        switch (ubyte) {
+                            case 0x9688:
+                                /* 'FULL BLOCK' (U+2588) */
+                                putchar(0xDB);
+                                break;
+                            case 0x9597:
+                                /* 'BOX DRAWINGS DOUBLE DOWN AND LEFT' (U+2557) */
+                                putchar(0xBB);
+                                break;
+                            case 0x9591:
+                                /* 'BOX DRAWINGS DOUBLE VERTICAL' (U+2551) */
+                                putchar(0xBA);
+                                break;
+                            case 0x9594:
+                                /* 'BOX DRAWINGS DOUBLE DOWN AND RIGHT' (U+2554) */
+                                putchar(0xC9);
+                                break;
+                            case 0x9590:
+                                /* 'BOX DRAWINGS DOUBLE HORIZONTAL' (U+2550) */
+                                putchar(0xCD);
+                                break;
+                            case 0x959d:
+                                /* 'BOX DRAWINGS DOUBLE UP AND LEFT' (U+255D) */
+                                putchar(0xBC);
+                                break; 
+                            case 0x959a:
+                                /* 'BOX DRAWINGS DOUBLE UP AND RIGHT' (U+255A) */
+                                putchar(0xC8);
+                                break; 
+                            default:
+                                fprintf(stderr, "UNHANDLED UBYTE VALUE = e2%04x\n", ubyte);
+                                exit(1);
+                                break;
+                            }
+
+                        unicode_on = false;
+                        unicode_byte_index = 0;
+                        ubyte = 0;
+                        }
+            } else {
+
+                switch(buffer[i]) {
+                case 0xE2:
+                    /* some freaky unicode magic symbol */
+                    fprintf(stderr, "\n[UNICODE=0x%02x]\n", buffer[i]);
+                    unicode_on = true;
+                    unicode_byte_index = 0;
+                    ubyte = 0;
+                    break;
+                default:
+                    if (buffer[i] >= 128) {
+                        fprintf(stderr,
+                                "+ Unicode error, buffer[%d] = 0x%02x!\n",
+                                i, buffer[i]);
+                        exit(1);
+                    }
+                    fprintf(stderr, "[0x%02x]", buffer[i]);
+                    putchar(buffer[i]);
+                    break;
+                }
+            }
+
+        }
+    }
+
+}
